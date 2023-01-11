9a579a04d5f263b212a98b79fee31bccd42e1a5b
Jonas Esser <mail@jonas-esser.de>
2023-01-11 19:14:46 +0100

    Update version to 0.1.0

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   @whal3s/whal3s.js v0.0.8
+ *   @whal3s/whal3s.js v0.1
  *   https://github.com/Whal3s-xyz/whal3s.js
  *
  *   Copyright (c) Jonas Esser (https://github.com/Whal3s-xyz) and project contributors.
