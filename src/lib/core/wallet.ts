647137ea1ae2a9356d071de978f9b0a3b75b537d
Jonas Esser <mail@jonas-esser.de>
2023-02-23 17:15:34 +0100

    FIX: Error after wallet got disconnected and connected again

--- src/lib/core/wallet.ts
+++ src/lib/core/wallet.ts
@@ -73,7 +73,7 @@ export class Wallet extends EventTarget {
             ...walletConfig
         });
         this.onboard.state.select('wallets').subscribe((update) => {
-            if (update[0]?.accounts[0]?.address !== this._lastWalletState?.accounts[0]?.address) {
+            if (update[0]?.accounts?.[0]?.address !== this._lastWalletState?.accounts?.[0]?.address) {
                 this._lastWalletState = {...update[0]}
                 this.dispatchEvent(new CustomEvent('addressChanged', {detail: {address: update[0]?.accounts[0]?.address}}))
             }
